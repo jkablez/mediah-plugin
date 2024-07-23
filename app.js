45ab5374e10de9a5444bfbf13551bef51db9b5a6
Jkable <jackablett008@gmail.com>
2024-07-23 02:41:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("Side Bar Tweaks")
 
 const anchor = document.createElement("a");
 anchor.href = "https://google.com/";
+anchor.textContent = "Super Cool Link"
 document.body.appendChild(anchor)
