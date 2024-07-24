6fb04215d1ba07eece4ff11826dc2ee8d2652340
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-24 23:49:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,69 +11,62 @@ if (location.pathname === "/dashboard") {
   this.showAlert("Injected Side Bar Tweaks!", "success");
 }
 
-async function init() {
-  var app = await import("/static/js/app.js")
-  const site = await app.site;
+class Handle {
+  constructor(site) {
+    this.site = site;
+    this.elements = {};
+    this.elements.root = this.createRoot();
 
-  class Handle {
-    constructor(site) {
-      this.site = site;
-      this.elements = {};
-      this.elements.root = this.createRoot();
+    this.sidebar = document.querySelector(".sidebar");
+    this.isHandleGrab = getStorage("isHandleGrab") || false;
 
-      this.sidebar = document.querySelector(".sidebar");
-      this.isHandleGrab = getStorage("isHandleGrab") || false;
+    console.log(this.isHandleGrab);
 
-      console.log(this.isHandleGrab);
-
-      this.elements.root.addEventListener("click", () => {
-        this.isHandleGrab = !this.isHandleGrab;
-        this.setWidth();
-        saveStorage("isHandleGrab", this.isHandleGrab);
-      });
-
+    this.elements.root.addEventListener("click", () => {
+      this.isHandleGrab = !this.isHandleGrab;
       this.setWidth();
+      saveStorage("isHandleGrab", this.isHandleGrab);
+    });
 
-      this.sidebar.appendChild(this.elements.root);
-    }
+    this.setWidth();
 
-    createRoot() {
-      const range = document.createRange();
-      range.selectNode(document.body);
-
-      return range.createContextualFragment(`
-            <button class="h-6 cursor-pointer text-muted-foreground rounded-md bg-background border flex items-center justify-center absolute -right-2 top-0 bottom-0 my-auto ${this.site.handleHidden ? 'hidden' : ''}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><circle cx="9" cy="12" r="1"/><circle cx="9" cy="5" r="1"/><circle cx="9" cy="19" r="1"/><circle cx="15" cy="12" r="1"/><circle cx="15" cy="5" r="1"/><circle cx="15" cy="19" r="1"/></svg>
-            </button>
-        `).firstElementChild;
-    }
-
-    setWidth() {
-      if (this.isHandleGrab) {
-        this.sidebar.style.left = "-270px";
-        this.sidebar.style.position = "absolute";
-      } else {
-        this.sidebar.style.left = "";
-        this.sidebar.style.position = "";
-      }
-    }
+    this.sidebar.appendChild(this.elements.root);
   }
 
-  const handleButton = new Handle({ handleHidden: getStorage("handleHidden") || false });
-  let handleHidden = getStorage("handleHidden") || false;
+  createRoot() {
+    const range = document.createRange();
+    range.selectNode(document.body);
 
-  document.querySelector("#toggle-handle").addEventListener("click", () => {
-    handleHidden = !handleHidden;
-    console.log(handleHidden);
+    return range.createContextualFragment(`
+          <button class="h-6 cursor-pointer text-muted-foreground rounded-md bg-background border flex items-center justify-center absolute -right-2 top-0 bottom-0 my-auto ${this.site.handleHidden ? 'hidden' : ''}">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4"><circle cx="9" cy="12" r="1"/><circle cx="9" cy="5" r="1"/><circle cx="9" cy="19" r="1"/><circle cx="15" cy="12" r="1"/><circle cx="15" cy="5" r="1"/><circle cx="15" cy="19" r="1"/></svg>
+          </button>
+      `).firstElementChild;
+  }
 
-    if (handleHidden) {
-      handleButton.elements.root.classList.add("hidden");
+  setWidth() {
+    if (this.isHandleGrab) {
+      this.sidebar.style.left = "-270px";
+      this.sidebar.style.position = "absolute";
     } else {
-      handleButton.elements.root.classList.remove("hidden");
+      this.sidebar.style.left = "";
+      this.sidebar.style.position = "";
     }
-
-    saveStorage("handleHidden", handleHidden);
-  });
+  }
 }
 
-init();
+const handleButton = new Handle({ handleHidden: getStorage("handleHidden") || false });
+let handleHidden = getStorage("handleHidden") || false;
+
+document.querySelector("#toggle-handle").addEventListener("click", () => {
+  handleHidden = !handleHidden;
+  console.log(handleHidden);
+
+  if (handleHidden) {
+    handleButton.elements.root.classList.add("hidden");
+  } else {
+    handleButton.elements.root.classList.remove("hidden");
+  }
+
+  saveStorage("handleHidden", handleHidden);
+});
