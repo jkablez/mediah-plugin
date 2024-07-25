9d54c04a2e9dd556b671b68ab523480ce130b7a3
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-25 03:08:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ class Handle {
   setWidth() {
     const sideBarWidth = 280;
     const collapseWidth = parseFloat(getStorage("collapseWidth")) || 10;
+    console.log(collapseWidth)
     const marginLeft = 0 - (sideBarWidth - collapseWidth);
     if (this.isHandleGrab) {
       this.sidebar.style.left = `${marginLeft}px`;
