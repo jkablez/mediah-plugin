f7b048ab2e3c2f2a9b5b90835b1a38ab9a7b3362
Alyc <126977575+jkablez@users.noreply.github.com>
2024-07-25 03:46:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ this.sideBarEdit("Side Bar Tweaks", [
   },
   {
     label: "Config",
-    href: "/dashboard/plugin/jkablez/mediah-plugin/side-bar-tweaks",
+    href: "/dashboard/plugin/jkablez/mediah-plugin/config",
     icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 -mt-[1px]"><path d="M20 7h-9"/><path d="M14 17H5"/><circle cx="17" cy="17" r="3"/><circle cx="7" cy="7" r="3"/></svg>`
   }
 ]);
@@ -89,7 +89,7 @@ class Page {
       event.preventDefault();
       const collapseWidth = this.form.querySelector("#collapseWidth");
       saveStorage("collapseWidth", parseFloat(collapseWidth.value) || 10);
-      showAlert("Saved Collapsed Width", "success");
+      showAlert("Saved Collapsed Width!", "success");
 
       if (getStorage("isHandleGrab")) {
         const marginLeft = 0 - (sideBarWidth - parseFloat(collapseWidth.value) || 10);
@@ -121,4 +121,4 @@ class Page {
   }
 }
 
-newPage("/side-bar-tweaks", "Side Bar Tweaks", Page);
+newPage("/config", "Side Bar Tweaks", Page);
